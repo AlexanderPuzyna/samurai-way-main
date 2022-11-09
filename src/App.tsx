6928bed6a8d16e7d211ab172b4471c4e902ba519
Alexander Puzyna <alexanderpuzyna@gmail.com>
2022-11-09 13:32:05 +0300

    lesson-11

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,42 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+
+const App = () => {
+    return (
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src='https://www.websitegrowth.com/wp-content/uploads/2016/08/apple-logo-300x300.png'/>
+            </header>
+            <nav className='nav'>
+                <div><a href="#">Profile</a></div>
+                <div><a href="#">Messages</a></div>
+                <div><a href="#">News</a></div>
+                <div><a href="#">Music</a></div>
+                <div><a href="#">Settings</a></div>
+
+            </nav>
+            <div className='content'>
+                <div>
+                    <img src='https://www.ukinbound.org/wp-content/uploads/2020/07/We-Are-Tourism-BCP-875x350.jpg'/>
+                </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New Post
+                    </div>
+                    <div>
+                        <div>post1</div>
+                        <div>post2</div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
