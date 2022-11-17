6a987bf54d65e76782613effe93a863cbd33bb11
Alexander Puzyna <alexanderpuzyna@gmail.com>
2022-11-17 17:42:27 +0300

    lesson-23

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -4,10 +4,12 @@ import Post from "./Post/Post";
 
 const MyPosts = () => {
     return (
-        <div>
-            My posts
+        <div className={s.postBlock}>
+            <h3>My posts</h3>
             <div>
-                <textarea></textarea>
+                <div>
+                    <textarea></textarea>
+                </div>
                 <button>Add Post</button>
             </div>
             <div className={s.posts}>
