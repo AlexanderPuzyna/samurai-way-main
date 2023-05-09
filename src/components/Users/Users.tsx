c68f31633dc14e33163f10158803b4c587ef2afb
Alexander Puzyna <alexanderpuzyna@gmail.com>
2023-05-09 16:04:25 +0300

    lesson_52

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -8,7 +8,6 @@ import userPhoto from '../../assets/images/user.png'
 const Users = (props: UsersContainerType) => {
     let getUsers = () => {
         if (props.users.length === 0) {
-
             axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
                 props.setUsers(response.data.items);
             });
